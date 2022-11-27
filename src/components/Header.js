125eb673966541477f379667d120afbc91034df5
rosellaliu <113400598+idleparrot567@users.noreply.github.com>
2022-11-26 20:40:46 -0500

    arranged well

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,29 @@
 import React from 'react';
+import BakeryDiningIcon from '@mui/icons-material/BakeryDining';
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Header(props){
     return (
         <header className='row block'>
-            <div>
-            <a href="#/">
-                <h1>The Idle Parrot's Bakery </h1>
-            </a>
-            </div>
-            <div>
-            <a href="#/">
-                <h1>Cart</h1>
-            </a>
-            </div>
-            <div>
-            <a href="#/">
-                <h1>Sign In</h1>
-            </a>
-            </div>
+        <div>
+        <a href="#/">
+            <h1>The Idle Parrot's Bakery </h1>
+        </a>
+        </div>
+        <div>
+        <a href="#/">
+            <h1>Cart</h1>
+        </a>
+        </div>
 
-        </header>
+
+    </header>
     );
 }
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,18 +1,34 @@
 import { Button } from '@mui/material';
 import React from 'react'
 
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
 export default function Product(props) {
     const {product, onAdd} = props;
   return (
-      <div className='product'>
-        <img className='small' src={product.image} alt={product.description}></img>
-        <div><h3>{product.name}</h3></div>
-        <div> ${product.price}</div>
-        <div>
-          <Button onClick= {() => {onAdd(product);}} variant="text">Add To Cart</Button>  
-          <Button onClick= {() => {onAdd(product);}} variant="text">Remove From Cart</Button>
-        </div>
-
-      </div>
-  )
+    <Card sx={{ maxWidth: 345 }}>
+    <CardMedia
+      component="img"
+      height="300"
+      image= {product.image}
+      alt="green iguana"
+    />
+    <CardContent>
+      <Typography gutterBottom variant="h5" component="div">
+        {product.name}
+      </Typography>
+      <Typography variant="body2" color="text.secondary">
+        {product.description}
+      </Typography>
+    </CardContent>
+    <CardActions>
+    <Button onClick= {() => {onAdd(product);}} variant="text">Add To Cart</Button>  
+      <Button onClick= {() => {onAdd(product);}} variant="text">Remove From Cart</Button>
+    </CardActions>
+  </Card>
+  );
 }
